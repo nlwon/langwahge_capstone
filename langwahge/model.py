913bed1a9339fc3fc59b4330fc2429ebce98a269
Bhargav <bhargav.panguluru@gmail.com>
2021-07-22 16:51:11 -0700

    model edit

--- langwahge/model.py
+++ langwahge/model.py
@@ -61,23 +61,27 @@ def loss_accuracy(sim_match, sim_confuse, threshold, triplets = 0):
 
 model = Model(input_dim = 512, output_dim = 200)
 optim = SGD(model.parameters, learning_rate = 1e-3, momentum = 0.9)
-
 from noggin import create_plot
 plotter, fig, ax = create_plot(metrics=["loss"], max_fraction_spent_plotting=.75)
 
 batch_size = 32
 
+data = coco_data()
+coco_data, glove, resnet18_features, imgid_to_capid, capid_to_imgid, capid_to_capstr, counters = data.get_self()
 #split the data
-for epoch in range(180):
-    indexes = np.arange((len(resnet_vectors)))
+training_vectors = resnet18_features[:61959]
+test_vectors = resnet18_features[61959:82600]
+
+for epoch in range(10000):
+    indexes = np.arange((len(training_vectors)))
     np.random.shuffle(indexes)
-    for batch_count in range(0,len(resnet_vectors)//batch_size):
+    for batch_count in range(0,len(training_vectors)//batch_size):
         batch_indexes = indexes[batch_count*batch_size: batch_count*(batch_size+1)]
-        batch = resnet_vectors[batch_indexes]  
-        #print(batch)
+        batch = training_vectors[batch_indexes]  
+#         print(batch)
         prediction = model(batch)
         
-        confuser = model(glove[random.choice(list(glove))])
+        confuser = model(resnet18_features[random.choice(list(resnet18_features)[:82600])])  
         
         sim_match = w_caption@prediction
         sim_confuse = w_caption@confuser
